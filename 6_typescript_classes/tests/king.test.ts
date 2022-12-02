d16e04868cd746266b1aa55ca515a90341b7cf23
karvaroz <vargasriosk@gmail.com>
2022-12-02 10:01:49 -0500

    queen and king tests

---
+++ 6_typescript_classes/tests/king.test.ts
@@ -0,0 +1,28 @@
+import Position from "../src/position";
+import King from '../src/king';
+
+beforeEach(() => {
+    king = new King("White", "E", 1);
+})
+
+describe("Test king", () => {
+	it("should move one place forward", () => {
+		let position = new Position("E", 2);
+		expect(king.canMoveTo(position).toBe(true));
+	});
+
+	it("shouldn't move to the same place", () => {
+		let position = new Position("E", 1);
+		expect(king.canMove(position).toBe(false));
+	});
+
+	it("should move one place to the left", () => {
+		let position = new Position("D", 1);
+		expect(king.canMoveTo(position).toBe(true));
+	});
+
+	it("shouldn't move forward more than 1 space", () => {
+		let position = new Position("E", 3);
+		expect(king.canMoveTo(position).toBe(false));
+	});
+});
---
+++ 6_typescript_classes/tests/queen.test.ts
@@ -0,0 +1,42 @@
+import Position from "../src/position";
+import Queen from "../src/queen";
+
+describe("Test queen", () => {
+	beforeEach(() => {
+		queen = new Queen("White", "D", 1);
+	});
+
+	it("should move vertically", () => {
+		let position = new Position("D", 8);
+		expect(queen.canMoveTo(position).toBe(true));
+	});
+
+	it("should move horizontally", () => {
+		let position = new Position("A", 1);
+		expect(queen.canMoveTo(position).toBe(true));
+	});
+
+	it("should move diagonally", () => {
+		let position = new Position("H", 5);
+		expect(queen.canMoveTo(position).toBe(true));
+
+		position = new Position("A", 4);
+		expect(queen.canMoveTo(position).toBe(true));
+	});
+
+	it("should not move L", () => {
+		let position = new Position("C", 3);
+		expect(queen.canMoveTo(position).toBe(false));
+
+		position = new Position("E", 3);
+		expect(queen.canMoveTo(position).toBe(false));
+	});
+
+	it("should not move other places", () => {
+		let position = new Position("C", 5);
+		expect(queen.canMoveTo(position).toBe(false));
+
+		position = new Position("F", 8);
+		expect(queen.canMoveTo(position).toBe(false));
+	});
+});
