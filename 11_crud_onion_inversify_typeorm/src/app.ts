539bd2acedca7d34451201f583425ea2c73d1ddf
Karina Vargas <karina.vargas@jala.university>
2022-12-09 12:55:31 -0500

    crud project

--- 11_crud_onion_inversify_typeorm/src/app.ts
+++ 11_crud_onion_inversify_typeorm/src/app.ts
@@ -9,23 +9,33 @@ class Test {
     await AppDataSource.initialize()
 
     const user = new UserEntity()
-    user.name = 'Juanes'
+    user.name = 'Camila'
     user.lastName = 'Nose'
-    user.email = 'juanes@gmail.com'
-    user.age = 20
-    user.job = 'singer'
+    user.email = 'camila@gmail.com'
+    user.age = 30
+    user.job = 'dancer'
+    console.log('[CREATE_USER] ', JSON.stringify(user))
 
     const dataAccess = new UserDatabase()
     await dataAccess.createUser(user)
+    const users = await dataAccess.readAll()
+    console.log('---------------------------------------------------------')
+    console.log('[ALL_USER] ', JSON.stringify(users))
+    console.log('---------------------------------------------------------')
 
-    const userLoaded = await dataAccess.readById(1)
+    const userLoaded = await dataAccess.readById(5)
+    console.log('[READ_USER_BY_ID] ', JSON.stringify(userLoaded))
+    console.log('---------------------------------------------------------')
 
     if (userLoaded) {
-      userLoaded.age = 30
+      userLoaded.name = 'Juan'
       dataAccess.updateUser(userLoaded)
+      console.log('[UPDATED_USER] ', JSON.stringify(userLoaded))
     }
+    console.log('---------------------------------------------------------')
 
-     dataAccess.deleteUser(1)
+    dataAccess.deleteUser(9)
+    console.log('[DELETED_USER] ', JSON.stringify(userLoaded))
   }
 }
 
--- 11_crud_onion_inversify_typeorm/src/infrastructure/user.database.ts
+++ 11_crud_onion_inversify_typeorm/src/infrastructure/user.database.ts
@@ -4,31 +4,26 @@ import { AppDataSource } from './user.dbsource'
 export default class UserDatabase {
   async readAll (): Promise<UserEntity[] | null> {
     const repository = AppDataSource.getRepository(UserEntity)
-    // console.log(repository)
     return await repository.find()
   }
 
   async readById (id: number): Promise<UserEntity | null> {
     const repository = AppDataSource.getRepository(UserEntity)
-    // console.log('Encontrado', id)
     return await repository.findOneBy({ id })
   }
 
   async createUser (user: UserEntity): Promise<UserEntity | null> {
     const repository = AppDataSource.getRepository(UserEntity)
-    // console.log('Creado', user)
     return await repository.save(user)
   }
 
   async updateUser (user: UserEntity): Promise<UserEntity | null> {
     const repository = AppDataSource.getRepository(UserEntity)
-    // console.log('Actualizado', user)
     return await repository.save(user)
   }
 
   async deleteUser (id: number): Promise<void> {
     const repository = AppDataSource.getRepository(UserEntity)
     await repository.delete({ id })
-    // console.log('Eliminado', id)
   }
 }
