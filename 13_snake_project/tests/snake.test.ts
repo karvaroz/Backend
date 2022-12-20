fdc40f018424f9ddd0a1bfcaeb5972b08c61e191
Karina Vargas <karina.vargas@jala.university>
2022-12-20 14:09:32 -0500

    testing snake service

--- 13_snake_project/tests/snake.test.ts
+++ 13_snake_project/tests/snake.test.ts
@@ -1,6 +1,6 @@
 import { AppDataSource } from "../src/infrastructure/database/app.dbsource";
 import SnakeEntity from "../src/infrastructure/database/snake/snake.entity";
-import SnakeDatabase from '../src/infrastructure/database/snake/snake.database';
+import SnakeDatabase from "../src/infrastructure/database/snake/snake.database";
 
 describe("SNAKE SERVICE TESTS", () => {
 	beforeAll(async () => {
@@ -8,17 +8,49 @@ describe("SNAKE SERVICE TESTS", () => {
 	});
 
 	it("should create a snake", async () => {
-		const snakeAccess = new SnakeDatabase()
+		const snakeAccess = new SnakeDatabase();
 		const snakeDB = new SnakeEntity();
-		snakeDB.snakeId = 20
-		snakeDB.snakeLength = 10
-		snakeDB.snakePositionX = 10
-		snakeDB.snakePositionY = 10
-		snakeDB.snakeDirection = "LEFT"
+		snakeDB.snakeId = 20;
+		snakeDB.snakeLength = 10;
+		snakeDB.snakePositionX = 10;
+		snakeDB.snakePositionY = 10;
+		snakeDB.snakeDirection = "LEFT";
 
 		const createSnake = await snakeAccess.createSnake(snakeDB);
-		expect(createSnake).not.toBeNull()
-		expect(createSnake.snakeId).toEqual(20)
+		expect(createSnake).not.toBeNull();
+		expect(createSnake.snakeId).toEqual(20);
+	});
 
+	it("should read read snake by ID", async () => {
+		const snakeAccess = new SnakeDatabase();
+		const snakeById = await snakeAccess.getSnakeById(10);
+		expect(snakeById).not.toBeNull();
+		expect(snakeById.snakeId).toEqual(10);
+	});
+
+	it("should update a snake", async () => {
+		const snakeAccess = new SnakeDatabase();
+		const getSnakeToUpdate = await snakeAccess.getSnakeById(10);
+
+		getSnakeToUpdate.snakeLength = 50;
+
+		expect(getSnakeToUpdate).not.toBeNull();
+		expect(getSnakeToUpdate.snakeLength).toEqual(50);
+	});
+
+	it("should be able to increase snake's lenght after it eats", async () => {
+		const snakeAccess = new SnakeDatabase();
+		const snake = await snakeAccess.getSnakeById(1);
+		
+		const pointsToEat: number = 10;
+		snake.snakeLength = snake.snakeLength + pointsToEat;
+
+		const updateSnake = await snakeAccess.updateSnake(snake);
+		const eatSnake = await snakeAccess.eatSnake(snake.snakeId, snake.snakeLength, snake);
+
+
+
+		expect(eatSnake).not.toBeNull();
+		//  expect(eatSnake.snakeLength).toEqual(snake.snakeLength + pointsToEat);
 	});
 });
