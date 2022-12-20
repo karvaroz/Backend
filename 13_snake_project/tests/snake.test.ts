8a5e8487e8e64fcebb066fabf5560a3cdeb14062
Karina Vargas <karina.vargas@jala.university>
2022-12-20 13:40:00 -0500

    one test running

--- 13_snake_project/tests/snake.test.ts
+++ 13_snake_project/tests/snake.test.ts
@@ -1,26 +1,24 @@
-import "reflect-metadata";
-import { Container } from "inversify";
+import { AppDataSource } from "../src/infrastructure/database/app.dbsource";
+import SnakeEntity from "../src/infrastructure/database/snake/snake.entity";
+import SnakeDatabase from '../src/infrastructure/database/snake/snake.database';
 
-import { SnakeService } from "../src/services/snake.services";
-import { SNAKESERVICE, SNAKE } from "../src/infrastructure/inversify/types";
+describe("SNAKE SERVICE TESTS", () => {
+	beforeAll(async () => {
+		await AppDataSource.initialize();
+	});
 
-import { SnakeRepository } from "../src/domain/repository/snake.repository";
-import SnakeDatabase from "../src/infrastructure/database/snake/snake.database";
-import { Snake } from "../src/domain/entities/snake.domain";
+	it("should create a snake", async () => {
+		const snakeAccess = new SnakeDatabase()
+		const snakeDB = new SnakeEntity();
+		snakeDB.snakeId = 20
+		snakeDB.snakeLength = 10
+		snakeDB.snakePositionX = 10
+		snakeDB.snakePositionY = 10
+		snakeDB.snakeDirection = "LEFT"
 
-let snakeService: SnakeService;
+		const createSnake = await snakeAccess.createSnake(snakeDB);
+		expect(createSnake).not.toBeNull()
+		expect(createSnake.snakeId).toEqual(20)
 
-beforeEach(() => {
-	const container = new Container();
-	container.bind<SnakeRepository>(SNAKESERVICE).to(SnakeService);
-	container.bind<SnakeRepository>(SNAKE).to(SnakeDatabase);
-	snakeService = container.get<SnakeService>(SNAKESERVICE);
-});
-
-test("Should create a snake", async () => {
-	const newSnake = await snakeService.createSnake(
-		new Snake(11, 1, 10, 15, "LEFT")
-	);
-
-	expect(newSnake).not.toBeNull();
+	});
 });
