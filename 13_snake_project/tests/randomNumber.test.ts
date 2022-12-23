8c7f2ea1a2511117ea067b0852d0cfca6aeca096
Karina Vargas <karina.vargas@jala.university>
2022-12-23 11:17:42 -0500

    postman collection

--- 13_snake_project/tests/randomNumber.test.ts
+++ 13_snake_project/tests/randomNumber.test.ts
@@ -2,7 +2,6 @@ import GenerateRandomNumber from "../src/services/utils/generateRandomNumber";
 
 
 describe("RANDOM NUMBER GENERATOR", () => {
-
     it("SHOULD GENERATE RANDOM NUMBER",  () => {
         const generator = new GenerateRandomNumber().randomNumber(10);
         expect(generator).toBeLessThanOrEqual(10)
