07e23f75667da772b15917928435e20c8a0aa070
Karina Vargas <karina.vargas@jala.university>
2023-02-02 19:16:08 -0500

    config and connection rabbitmq all services

--- 15_final_project/stats/src/app.ts
+++ 15_final_project/stats/src/app.ts
@@ -1,11 +1,28 @@
-import express from "express"
-const app = express();
-const port = 3000;
+import express from 'express'
+import 'reflect-metadata'
+import amqp = require('amqplib/callback_api')
 
-app.get("/", (req: any, res: any) => {
-    res.send("Hello World!");
-});
+const app = express()
+const port = 3000
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
+
+amqp.connect({
+  protocol: 'amqp',
+  hostname: 'KARINA-LAPTOP',
+  port: 5672,
+  username: 'admin',
+  password: 'pass123'
+}, function (error0, connection) {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (error0) {
+    throw error0
+  }
+  console.log(connection)
+})
--- 15_final_project/uploader/src/app.ts
+++ 15_final_project/uploader/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'reflect-metadata'
+import amqp = require('amqplib/callback_api')
 
 const app = express()
 const port = 3000
@@ -10,4 +11,18 @@ app.get('/', (req: any, res: any) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+amqp.connect({
+  protocol: 'amqp',
+  hostname: 'KARINA-LAPTOP',
+  port: 5672,
+  username: 'admin',
+  password: 'pass123'
+}, function (error0, connection) {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (error0) {
+    throw error0
+  }
+  console.log(connection)
 })
